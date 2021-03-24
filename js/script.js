a78ab7faf7fc5c88233f575119fb9ac211c56ba2
GioOli7 <gio07live@gmail.com>
2021-03-24 18:39:33 +0100

    top

--- js/script.js
+++ js/script.js
@@ -10,32 +10,32 @@
 
 // array con lista cognomi
 var nomeList = ['bianchi', 'neri', 'rossi', 'verdi', 'gialli'];
-console.log('array iniziale: ' + nomeList);
+console.log(`array iniziale: ${nomeList}`);
 
 // 1 - chiedo cognome utente e verifico che non sia stringa vuota
 do {
     var cognome = prompt("Inserisci il tuo nome").toLowerCase().trim();
 } while (cognome === '');
-console.log('Cognome inserito: ' + cognome)
-
+console.log(`Cognome inserito: ${cognome}`);
+ 
 // 2 - inserisco cognome in array
 nomeList.push(cognome)
-console.log('nuova array: ' + nomeList);
+console.log(`nuova array: ${nomeList}`);
 
 // 3 - ordino e stampo array in ordine alfabetico
 nomeList.sort();
-console.log('array ordinata alfabeticamente: ' + nomeList);
+console.log(`array ordinata alfabeticamente: ${nomeList}`);
 
 // 4 - stampo posizione 'umana' del cognome che ho inserito nell array ordinata
 var pos = nomeList.indexOf(cognome);
 pos += 1;
-console.log('posizione: ' + pos);
+console.log(`posizione: ${pos}`);
 
 // inserisco la lista cognomi in HTML
 var items = '';
 
 for (var i = 0; i < nomeList.length; i++) {
-    items += '<li>' + nomeList[i] + '</li>';
+    items += `<li>${nomeList[i]}</li>`;
 }
 document.getElementById('surname_list').innerHTML = items;
 
